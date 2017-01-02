9322a9081a4ed40b681ff03faa7cb90a501d04f0
vercruyssetim <tim.vercruysse@cegeka.be>
2017-01-02 12:37:08 +0100

    [Tim] - added api.ai - added web.server.js

--- src/ai/api.service.js
+++ src/ai/api.service.js
@@ -1,12 +1,15 @@
 const apiai = require('apiai');
+const webserver = require('../server/web.server');
 
 class ApiService {
-    constructor(apiai) {
+    constructor(apiai, webserver) {
         this.apiai = apiai;
+        this.webserver = webserver;
     }
 
     $onInit() {
         this.app = apiai("a00b99c7210944678bfdabb7bbb29dd6");
+        this.initWebhook(this.webserver);
     }
 
     sendRequest(text, sessionId, sender) {
@@ -23,9 +26,32 @@ class ApiService {
         });
         request.end();
     }
+
+    initWebhook(webserver) {
+        webserver.server.post('/api/webhook', (req, res) => {
+            let action = req.body.result.action;
+            let parameters = req.body.result.parameters;
+
+            if(action === 'getForecast'){
+                let location = parameters['geo-capital'];
+                let message = `The weather will be sunny in ${location}`;
+                res.send({
+                    "speech": message,
+                    "displayText": message,
+                    "source": "apiai-weather-webhook-sample"
+                })
+            } else {
+                res.send({
+                    "speech": 'I\'m gonna help you!',
+                    "displayText": 'I\'m gonna help you!',
+                    "source": "apiai-weather-webhook-sample"
+                });
+            }
+        })
+    }
 }
 
-const apiService = new ApiService(apiai);
+const apiService = new ApiService(apiai, webserver);
 apiService.$onInit();
 module.exports = apiService;
 
