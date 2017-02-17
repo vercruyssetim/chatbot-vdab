66ffe00323bf072fe92a1670075a6b3d7b8c3f53
vercruyssetim <tim.vercruysse@cegeka.be>
2017-02-17 14:03:54 +0100

    [Tim] - using new mocha reporter

--- gulp-tasks/test/mocha.js
+++ gulp-tasks/test/mocha.js
@@ -1,28 +1,15 @@
 import gulp from 'gulp';
 import mocha from 'gulp-mocha';
 
-export const testunitDev = 'test:dev:unit';
-export const testunitCI = 'test:ci:unit';
+export const testunit = 'test:unit';
 
-
-gulp.task(testunitDev, () => {
+gulp.task(testunit, () => {
     return gulp.src('src/**/*_test.js', {read: false})
         .pipe(mocha({
             useColors: false,
-        }))
-        .on('error', (err) => {
-            console.log(err.toString());
-            this.emit('end');
-        });
-});
-
-gulp.task(testunitCI, () => {
-    return gulp.src('src/**/*_test.js', {read: false})
-        .pipe(mocha({
-            useColors: false,
-            reporter: 'mocha-junit-reporter',
+            reporter: 'mocha-jenkins-reporter',
             reporterOptions: {
-                mochaFile: './test-reports/mocha/test-results_mocha.xml'
+                junit_report_path: './test-reports/mocha/test-results_mocha.xml'
             }
         }))
         .on('error', (err) => {
--- gulp-tasks/test/test.js
+++ gulp-tasks/test/test.js
@@ -1,31 +1,14 @@
 import gulp from 'gulp';
 import runSequence from 'run-sequence';
-import {testunitDev, testunitCI} from './mocha';
+import {testunit} from './mocha';
 import {eslint} from './eslint';
 
 export const test = 'test';
-export const testDev = 'test:dev';
-export const testCI = 'test:ci';
-
-gulp.task(testDev, (callback) => {
-    runSequence(
-        eslint,
-        testunitDev,
-        callback
-    );
-});
-
-gulp.task(testCI, (callback) => {
-    runSequence(
-        eslint,
-        testunitCI,
-        callback
-    );
-});
 
 gulp.task(test, (callback) => {
     runSequence(
-        testunitDev,
+        eslint,
+        testunit,
         callback
     );
 });
